0ca07444ad875adbf931f2f78cb13e240016d72a
= <ezrawyoung@gmail.com>
2021-12-10 23:36:55 -0500

    implement progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,59 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const heightLookup = {
+  small: 8,
+  medium: 12,
+  large: 24,
 };
+const radiusLookup = {
+  small: 4,
+  medium: 4,
+  large: 8,
+}
+
+/**
+ * 
+ * @param {Object} props
+ * @param {number} props.value
+ * @param {"small"|"medium"|"large"} props.size
+ */
+const ProgressBar = ({ value, size }) => {
+  const height = heightLookup[size];
+  const radius = radiusLookup[size];
+  const padding = size === "large" ? 4 : 0;
+
+  return (
+    <Wrapper height={height} radius={radius} padding={padding} role="progressbar" aria-valuenow={value} >
+      <FillWrapper>
+        <Fill value={value} />
+      </FillWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  overflow: hidden;
+  background-color: ${COLORS.transparentGray15};
+  width: 100%;
+  max-width: 370px;
+  height: ${props => props.height}px;
+  border-radius: ${props => props.radius}px;
+  padding: ${props => props.padding}px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const FillWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
+`;
+
+const Fill = styled.div`
+  background-color: ${COLORS.primary};
+  height: 100%;
+  width: ${props => props.value}%;
+`;
 
 export default ProgressBar;
